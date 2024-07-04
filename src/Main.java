194cd974b06a1aef4411693edac12d5636631e06
Alisson Viteri <alisson.viteri01@epn.edu.ec>
2024-07-04 08:55:22 -0500

    vesrion4

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -11,12 +12,28 @@ public class Main {
         try (Connection connection = DriverManager.getConnection(url,user,password)){
             System.out.println("Conectado a la base de datos");
             String query = "SELECT * FROM estudiante";
+               /*
+            Para buscar algo que el usuario solicite  */
+            Scanner sc =  new Scanner(System.in);
+            System.out.printf("Ingrese un nombre de usuario: ");
+            String nombre = sc.nextLine();
+            String query2 = "SELECT * FROM estudiante WHERE nombre = '" + nombre + "'";
+
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
             while (resultSet.next()){
                 System.out.println("Nombre: "+resultSet.getString("nombre"));
                 System.out.println("Nota b1: "+resultSet.getString("b1")); //b1: es el nombre de la tabla en mysql
                 System.out.println("Nota b2: "+resultSet.getString("b2"));
+
+                Double b1 = resultSet.getDouble("b1");
+                Double b2 = resultSet.getDouble("b2");
+                Double b3 = b1+b2;
+                if(b3>28){
+                    System.out.println("paso");
+                }else{
+                    System.out.println("No paso");
+                }
 
             }
         }
